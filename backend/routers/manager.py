92a4b69a7a6faf004ba93d0e618434fca8160a95
Rehan Anjum <syedrehananjum@gmail.com>
2024-11-24 02:33:19 -0500

    Add file upload for manager endpoint

--- backend/routers/manager.py
+++ backend/routers/manager.py
@@ -1,7 +1,15 @@
-from typing import Optional
-from fastapi import APIRouter
+from typing import Optional, Dict
+from fastapi import (
+    APIRouter, 
+    File,
+    UploadFile, 
+    HTTPException
+)
 
-from ..utils import get_db, get_llm
+from ..utils import (
+    get_db, 
+    get_llm
+)
 
 router = APIRouter(
     prefix='/manager',
@@ -9,15 +17,29 @@ router = APIRouter(
 )
 
 @router.post("/create")
-def create(doc: str, metadata: Optional[str]) -> str:
-    db = get_db()
-    return f"Uploaded to Corpus."
+def create(doc_name: str, file: UploadFile = File(...)) -> Dict[str, str]:
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        file.file.close()
+
+    return {
+        "message": f"Successfully uploaded {file.filename}"
+    }
 
 @router.post("/delete")
-def delete(name: str) -> str:
-    return f"Deleted {name} from Corpus."
+def delete(name: str) -> Dict[str, str]:
+    return {
+        "message": f"Deleted {name} from Corpus."
+    }
 
 @router.post("/update")
-def update(doc_name: str, new_metadata: str):
-    return f"Edited metadata for document: {doc_name}"
+def update(doc_name: str, new_metadata: str) -> Dict[str, str]:
+    return {
+        "message": f"Edited metadata for document: {doc_name}"
+    }
 
